5c7aafecefa4c765046d336b24f27e0708beb922
sioutis <mitsosgr96@gmail.com>
2024-11-10 20:32:42 +0200

    X

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const About = () => {
   return (
     <div className='text-sm'>
-    From a young age, I've been drawn to the world of design. What started as a love for drawing and creative expression soon merged with a fascination for technology, leading me to the dynamic field of full-stack web development. Now, I’m passionate about crafting visually engaging and efficient digital experiences, bringing together form and function to create seamless applications.
+    From a young age, I&apos;ve been drawn to the world of design. What started as a love for drawing and creative expression soon merged with a fascination for technology, leading me to the dynamic field of full-stack web development. Now, I&apos;m passionate about crafting visually engaging and efficient digital experiences, bringing together form and function to create seamless applications.
     <br/><br/>
-    I hold a degree in Computer Science from Harokopeio University of Athens, where I gained a broad foundation in multiple programming languages but found my passion in web development. My thesis, titled <i className='text-slate-300'>"Evaluation and Utilization of World Wide Web Technologies for the Design and Development of a Content Management System"</i> focused on using modern web tools to enhance content management.
+    I hold a degree in Computer Science from Harokopeio University of Athens, where I gained a broad foundation in multiple programming languages but found my passion in web development. My thesis, titled <i className='text-slate-300'>&quot;Evaluation and Utilization of World Wide Web Technologies for the Design and Development of a Content Management System&quot;</i> focused on using modern web tools to enhance content management.
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import "./globals.css";
-import Nav from "./components/Nav";
-
 
 export default function RootLayout({
   children,
