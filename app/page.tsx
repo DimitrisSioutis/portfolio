b10dc6834f3d305f0d2e70a2cd375dd3f4152f49
sioutis <mitsosgr96@gmail.com>
2024-11-10 20:19:28 +0200

    observer

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,14 @@ import Brackets from "./components/Brackets";
 import Experience from "./components/Experience";
 import Nav from "./components/Nav";
 
-interface IntersectionOptions {
-  threshold?: number;
-}
-
-const useIntersectionObserver = (options: IntersectionOptions = {}) => {
+const useIntersectionObserver = () => {
   const [inView, setInView] = useState(false);
   const ref = useRef<HTMLElement | null>(null);
-  const { threshold = 0 } = options;  // Destructure with default value
 
   useEffect(() => {
     const observer = new IntersectionObserver(([entry]) => {
       setInView(entry.isIntersecting);
-    }, { threshold });
+    }, { threshold: 0 });
 
     const currentRef = ref.current;
     
@@ -30,7 +25,7 @@ const useIntersectionObserver = (options: IntersectionOptions = {}) => {
         observer.unobserve(currentRef);
       }
     };
-  }, [threshold]);  // Use destructured threshold in dependency array
+  }, []); 
 
   return { ref, inView };
 };
@@ -38,9 +33,9 @@ const useIntersectionObserver = (options: IntersectionOptions = {}) => {
 export default function Home() {
   const [activeSection, setActiveSection] = useState<string>("about");
 
-  const { ref: aboutRef, inView: aboutInView } = useIntersectionObserver({ threshold: 0 });
-  const { ref: experienceRef, inView: experienceInView } = useIntersectionObserver({ threshold: 0 });
-  // const { ref: projectsRef, inView: projectsInView } = useIntersectionObserver({ threshold: 0 });
+  const { ref: aboutRef, inView: aboutInView } = useIntersectionObserver();
+  const { ref: experienceRef, inView: experienceInView } = useIntersectionObserver();
+  // const { ref: projectsRef, inView: projectsInView } = useIntersectionObserver();
 
   useEffect(() => {
     if (aboutInView) setActiveSection("about");
@@ -49,7 +44,7 @@ export default function Home() {
   }, [aboutInView, experienceInView]);
 
   return (
-    <main className="lg:flex justify-center lg:px-40 md:py-32 bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(15,23,42,1),rgba(2,6,23,0))]">
+    <main className="lg:flex justify-center lg:px-40 md:py-24 bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(15,23,42,1),rgba(2,6,23,0))]">
       <div className="min-w-[500px] flex-1">
         <Nav activeSection={activeSection} />
       </div>
