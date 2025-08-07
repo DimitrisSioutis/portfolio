9129313f318edb45e7a4c1006f137eee5df48268
Dimitris Sioutis <dsioutis@plaisio.gr>
2025-08-07 11:16:04 +0300

    removed unused useRef

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect} from "react";
 import { useInView } from "react-intersection-observer";
 import About from "./components/About";
 import Brackets from "./components/Brackets";
